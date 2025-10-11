8684c2543e10d69160059dd292cf002ba946816c
kub938 <kub938@naver.com>
2025-10-11 17:55:45 +0900

    feat: Home 구성

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,3 +1,13 @@
+import HeroSection from "@/components/HeroSection/HeroSection";
+import Intro from "@/components/Intro/Intro";
+import ProjectSection from "@/components/ProjectsSection/ProjectsSection";
+
 export default function Home() {
-  return <main className="main"></main>;
+  return (
+    <main className="main" style={{ position: "relative" }}>
+      <Intro></Intro>
+      <HeroSection></HeroSection>
+      <ProjectSection></ProjectSection>
+    </main>
+  );
 }
