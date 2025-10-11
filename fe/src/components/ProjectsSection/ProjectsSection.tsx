a528a7f2983cbb1fb64be245d917a7fe87f13118
kub938 <kub938@naver.com>
2025-10-11 17:58:27 +0900

    feat: Project Section 구현

--- fe/src/components/ProjectsSection/ProjectsSection.tsx
+++ fe/src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,5 +1,21 @@
+import ProjectCard from "./ProjectCard";
+import styles from "./ProjectsSection.module.css";
+
 function ProjectSection() {
-  return <></>;
+  return (
+    <section className="sectionY">
+      <div className={styles["project-section-header"]}>
+        <h2 className={styles["project-title"]}>PROJECT</h2>
+        <p className={styles["project-content"]}>
+          제가 진행한 3가지 프로젝트를 소개해 드립니다
+        </p>
+      </div>
+
+      <div>
+        <ProjectCard />
+      </div>
+    </section>
+  );
 }
 
 export default ProjectSection;
